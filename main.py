4287423d0b3d5ee96cc8be3d52da8467ca504dc5
Yuet Au Yeung <me@yuetau.net>
2022-03-31 16:38:27 +0800

    Update features

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import time
 import os
 from PIL import Image,ImageDraw,ImageFont
 from string import Template
+import signal
 
-import pyModeS as pms
-from pyModeS.extra.tcpclient import TcpClient
+import httpx
 
 import threading
 
@@ -17,6 +17,12 @@ font_reg_10 = ImageFont.truetype("fonts/Menlo-Regular.ttf", 10)
 
 font_bold_12 = ImageFont.truetype("fonts/Menlo-Bold.ttf", 12)
 font_reg_12 = ImageFont.truetype("fonts/Menlo-Regular.ttf", 12)
+
+font_bold_14 = ImageFont.truetype("fonts/Menlo-Bold.ttf", 14)
+font_reg_14 = ImageFont.truetype("fonts/Menlo-Regular.ttf", 14)
+
+font_bold_16 = ImageFont.truetype("fonts/Menlo-Bold.ttf", 16)
+font_reg_16 = ImageFont.truetype("fonts/Menlo-Regular.ttf", 16)
 
 boot_time = datetime.strptime(os.popen('uptime -s').read().strip(), "%Y-%m-%d %X")
 
@@ -28,12 +34,18 @@ spacing_height_12 = 12
 spacing_width_12 = 8
 offset_12 = 7
 
-planes_count = 0
-
-station_pos =  (22.44913, 114.16835)
 
 class DeltaTemplate(Template):
     delimiter ="%"
+
+class GracefulKiller:
+  kill_now = False
+  def __init__(self):
+    signal.signal(signal.SIGINT, self.exit_gracefully)
+    signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+  def exit_gracefully(self,signum, frame):
+    self.kill_now = True
 
 def strfdelta(tdelta, fmt):
     d = {"D": tdelta.days}
@@ -50,75 +62,114 @@ def strfdelta(tdelta, fmt):
     t = DeltaTemplate(fmt)
     return t.substitute(**d)
 
+bmp = Image.open("pics/paper-plane-white.bmp")
+all_planes = {}
+total_planes = 0
+
 def frame_boarder(image, draw, height, width):
     global planes_count
     #Title Bar
     draw.text((0, 0), "ADS-B Monitor", font=font_bold_10)
     draw.line([(0, (spacing_height_10*1)+offset_10), (width, (spacing_height_10*1)+offset_10)], width=1)
 
+    #Plane
+    image.paste(bmp, (offset_10, 38))
+    draw.line([(65, (spacing_height_10*1)+offset_10), (65, height-spacing_height_10*1-offset_10)], width=1)
+
+    #Dynamic
+
     #Uptime
     current_time = datetime.now()
     time_delta = current_time - boot_time
     timespan = strfdelta(time_delta,"%H:%M:%S")
     draw.text((width-spacing_width_10*3-spacing_width_10*len(timespan), 0), "UP", font = font_bold_10)
-    draw.text((width-spacing_width_10*len(timespan), 0), strfdelta(time_delta,"%H:%M:%S"), font = font_reg_10)
-
-    #Plane
-    bmp = Image.open("pics/paper-plane-white.bmp")
-    image.paste(bmp, (offset_10, 38))
-    draw.line([(65, (spacing_height_10*1)+offset_10), (65, height-spacing_height_10*1-offset_10)], width=1)
+    draw.text((width-spacing_width_10*len(timespan), 0), timespan, font = font_reg_10)
 
     #Bottom Bar
     draw.text((0, height-spacing_height_10*1), "TOTAL", font=font_bold_10)
-    draw.text((spacing_width_10*6, height-spacing_height_10*1), str(planes_count), font=font_reg_10)
+    draw.text((spacing_width_10*6, height-spacing_height_10*1), str(total_planes), font=font_reg_10)
     draw.line([(0, height-spacing_height_10*1-offset_10), (width, height-spacing_height_10*1-offset_10)], width=1)
 
-planes = {}
-planes_lock = False
+    hostname_cmd = os.popen("hostname -I").read().split()
+    if len(hostname_cmd) >= 1:
+        local_ip = hostname_cmd[0]
+    else:
+        local_ip = "Unable"
+    ssid_cmd = os.popen("iwgetid").read().split('"')
+    if len(ssid_cmd) >= 2:
+        ssid = ssid_cmd[1]
+    else:
+        ssid = "NO WIFI"
+    draw.text((width-spacing_width_10*len(local_ip), height-spacing_height_10*1), local_ip, font = font_reg_10)
+    draw.text((width-spacing_width_10*len(local_ip)-spacing_width_10*(len(ssid)+1), height-spacing_height_10*1), ssid, font = font_bold_10)
+
+client = httpx.Client(http2=True, base_url="http://127.0.0.1/tar1090")
 
 def frame_planes(image, draw, height, width):
-    global planes_lock
+    global all_planes
+    global total_planes
+    global client
+    planes = []
+    r = client.get("/data/aircraft.json")
+    if r.status_code == httpx.codes.OK:
+        json = r.json()
+        planes = json["aircraft"]
     i = 0
-    while planes_lock:
-        time.sleep(0.2)
-    planes_lock = True
-    for plane in list(planes.keys()):
-        if (datetime.now().timestamp()-planes[plane]["lc"]) > 300.0:
-            planes.pop(plane)
-            continue
+    for plane in list(all_planes.keys()):
 
-        draw.text((65+offset_12, (spacing_height_10*1)+offset_10*2+(spacing_height_12*i)+offset_12*i), plane, font = font_bold_12)
-        if "cs" in planes[plane]:
-            draw.text((65+offset_12+spacing_width_12*6+offset_12, (spacing_height_10*1)+offset_10*2+(spacing_height_12*i)+offset_12*i), planes[plane]["cs"], font = font_reg_12)
-        if "alt" in planes[plane]:
-            draw.text((65+offset_12+spacing_width_12*6+offset_12+spacing_width_12*7+offset_12, (spacing_height_10*1)+offset_10*2+(spacing_height_12*i)+offset_12*i), str(planes[plane]["alt"]), font = font_reg_12)
-        i = i+1
-    planes_lock = False
+        if (datetime.now().timestamp() - all_planes[plane]["lc"]) > 3600.0:
+            all_planes.pop(plane)
 
-last_update_frame = 0
-display_lock = False
+    for plane in planes:
+
+        if plane["hex"] not in all_planes:
+            all_planes[plane["hex"]] = {}
+            total_planes = total_planes + 1
+
+
+        if i > 5:
+            break
+
+        offset_x = 65+offset_12
+        offset_y = (spacing_height_10*1)+offset_10*2+(spacing_height_12*i)+offset_10*i
+        draw.text((offset_x, offset_y), plane["hex"].upper(), font = font_bold_12)
+
+        offset_x = offset_x + spacing_width_12*6+offset_10
+        if "flight" in plane:
+            draw.text((offset_x, offset_y), plane["flight"].strip().upper(), font = font_reg_12)
+
+        offset_x = offset_x + spacing_width_12*7+offset_10
+        if "alt_baro" in plane:
+            draw.text((offset_x, offset_y), str(plane["alt_baro"]), font = font_reg_12)
+
+            if "baro_rate" not in plane:
+                if "alt" in all_planes[plane["hex"]]:
+                    plane["baro_rate"] = (plane["alt_baro"] - all_planes[plane["hex"]]["alt"])/(datetime.now().timestamp()-all_planes[plane["hex"]]["lc"])
+
+            all_planes[plane["hex"]]["alt"] = plane["alt_baro"]
+
+        offset_x = offset_x + spacing_width_12*5+offset_10
+
+        if "baro_rate" in plane:
+            if plane["baro_rate"] > 0:
+                draw.text((offset_x, offset_y-3), "↑", font = font_bold_16)
+            elif plane["baro_rate"] == 0:
+                draw.text((offset_x, offset_y-3), "-", font = font_bold_16)
+            elif plane["baro_rate"] < 0:
+                draw.text((offset_x, offset_y-3), "↓", font = font_bold_16)
+
+
+        all_planes[plane["hex"]]["lc"] = datetime.now().timestamp()
+        i = i + 1
 
 def frame(epd):
-    global last_update_frame
-    global display_lock
-    if (datetime.now().timestamp() - last_update_frame) < 5.0:
-        return False
-    last_update_frame = datetime.now().timestamp()
     print("Framing")
     image = Image.new('1', (epd.height, epd.width), 255)
     draw = ImageDraw.Draw(image)
     frame_boarder(image, draw, epd.width, epd.height)
     frame_planes(image, draw, epd.width, epd.height)
-    while display_lock:
-        time.sleep(0.2)
-    display_lock = True
     epd.displayPartial(epd.getbuffer(image))
-    display_lock = False
     return True
-
-def frame_timer(epd):
-    print("Timer Framing")
-    frame(epd)
 
 epd = epd2in13_V2.EPD()
 print("Init and Clear")
@@ -128,99 +179,9 @@ epd.Clear(0xFF)
 epd.init(epd.PART_UPDATE)
 frame(epd)
 
-class ContinousTimer():
-    def __init__(self):
-        self.timer = None
-
-    def run(self, epd):
-        frame_timer(epd)
-
-        self.timer = threading.Timer(interval=10, function=self.run, args=(epd, ))
-        self.timer.start()
-
-t = ContinousTimer()
-t.run(epd=epd)
-
-class ADSBClient(TcpClient):
-    def __init__(self, host, port, rawtype):
-        super(ADSBClient, self).__init__(host, port, rawtype)
-
-    def handle_messages(self, messages):
-        global planes_count
-        global planes_lock
-        for msg, ts in messages:
-            if len(msg) != 28:  # wrong data length
-                continue
-            df = pms.df(msg)
-            if df != 17:  # not ADSB
-                continue
-            if pms.crc(msg) !=0:  # CRC fail
-                continue
-            icao = pms.adsb.icao(msg)
-            tc = pms.adsb.typecode(msg)
-            #time = datetime.fromtimestamp(ts)
-            while planes_lock:
-                time.sleep(0.2)
-            planes_lock = True
-            if icao not in planes:
-                planes[icao] = {"lc": ts, "msg": [], "weather": {}}
-                planes_count = planes_count + 1
-
-            if 1 <= tc and tc <= 4:
-                planes[icao]["cs"] = pms.adsb.callsign(msg).strip("_")
-            elif (5 <= tc and tc <= 18) or (20 <= tc and tc <= 22):
-                flag = pms.adsb.oe_flag(msg)
-                i = 0
-                for bmsg in planes[icao]["msg"]:
-                    if (datetime.now().timestamp()-bmsg[1]) > 10.0:
-                        planes[icao]["msg"].pop(i)
-                        i = i + 1
-                        continue
-                    if bmsg[2] != flag:
-                        if bmsg[2] == 0:
-                            even = bmsg
-                            odd = (msg, ts, flag)
-                        else:
-                            odd = bmsg
-                            even = (msg, ts, flag)
-                        lat, lon = pms.adsb.position(even[0], odd[0], even[1], odd[1], station_pos[0], station_pos[1])
-                        planes[icao]["lat"] = lat
-                        planes[icao]["lon"] = lon
-                    i = i + 1
-                planes[icao]["alt"] = pms.adsb.altitude(msg)
-                planes[icao]["msg"].append((msg, ts, flag))
-            elif tc == 19:
-                speed, angle, vertical, st = pms.adsb.velocity(msg)
-                planes[icao]["sp"] = speed
-                planes[icao]["ag"] = angle
-                planes[icao]["vr"] = vertical
-                planes[icao]["st"] = st
-
-            if pms.bds.bds40.is40(msg):
-                planes[icao]["altfms"] = pms.bds.bds40.selalt40fms(msg)
-                planes[icao]["altmcp"] = pms.bds.bds40.selalt40mcp(msg)
-
-            if pms.bds.bds44.is44(msg):
-                planes[icao]["weather"]["humd"] = pms.bds.bds44.hum44(msg)
-                planes[icao]["weather"]["temp"] = pms.bds.bds44.temp44(msg)
-                planes[icao]["weather"]["turb"] = pms.bds.bds44.turb44(msg)
-                planes[icao]["weather"]["wind"] = pms.bds.bds44.wind44(msg)
-
-            if pms.bds.bds45.is45(msg):
-                planes[icao]["weather"]["icing"] = pms.bds.bds45.ic45(msg)
-                planes[icao]["weather"]["mburst"] = pms.bds.bds45.mb45(msg)
-                planes[icao]["weather"]["windshear"] = pms.bds.bds45.ws45(msg)
-                planes[icao]["weather"]["wakevortex"] = pms.bds.bds45.wv45(msg)
-
-            if pms.bds.bds50.is50(msg):
-                planes[icao]["roll"] = pms.bds.bds50.roll50(msg)
-
-            if pms.bds.bds60.is60(msg):
-                planes[icao]["mach"] = pms.bds.bds60.mach60(msg)
-
-            planes_lock = False
-            print(icao, tc, time.strftime("%H:%M:%S"), planes[icao])
-            frame(epd)
-
-client = ADSBClient(host='127.0.0.1', port=30005, rawtype='beast')
-client.run()
+killer = GracefulKiller()
+while not killer.kill_now:
+    frame(epd)
+    time.sleep(1)
+epd.sleep()
+client.close()
