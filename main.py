35e1f162703657346e3366d5120cbb1277c709ee
Yuet Au Yeung <me@yuetau.net>
2022-04-02 18:33:11 +0800

    Render Boarder and SSID static

--- main.py
+++ main.py
@@ -63,16 +63,89 @@ def strfdelta(tdelta, fmt):
     return t.substitute(**d)
 
 bmp = Image.open("pics/paper-plane-white.bmp")
+
+static_image = None
+
+def frame_static(height, width):
+    global static_image
+    if not static_image:
+        image = Image.new('1', (width, height), 255)
+        draw = ImageDraw.Draw(image)
+
+        #Title Bar
+
+        draw.text((0, 0), "ADS-B Monitor", font=font_bold_10)
+        draw.line([(0, (spacing_height_10*1)+offset_10), (width, (spacing_height_10*1)+offset_10)], width=1)
+
+        #Plane Image
+        image.paste(bmp, (offset_10, 38))
+        draw.line([(65, (spacing_height_10*1)+offset_10), (65, height-spacing_height_10*1-offset_10)], width=1)
+
+        #Bottom Bar
+        draw.line([(0, height-spacing_height_10*1-offset_10), (width, height-spacing_height_10*1-offset_10)], width=1)
+
+        static_image = image.copy()
+        return image, draw
+
+    else:
+        image = static_image.copy()
+        draw = ImageDraw.Draw(image)
+        return image, draw
+
+last_ssid = None
+last_local_ip = None
+static_network = None
+
+def draw_ssid(image, draw, height, width, ssid, local_ip):
+
+    bb_offset_x = width-spacing_width_10*len(local_ip)
+    bb_offset_y = height-spacing_height_10*1
+    draw.text((bb_offset_x, bb_offset_y), local_ip, font = font_reg_10)
+    bb_offset_x = bb_offset_x - spacing_width_10*(len(ssid)+1)
+    draw.text((bb_offset_x, bb_offset_y), ssid, font = font_bold_10)
+
+    static_network = image.copy()
+    return image, draw
+
+def frame_ssid(image, draw, height, width):
+    global last_ssid
+    global last_local_ip
+    global static_network
+    local_ip = None
+    ssid = None
+    hostname_cmd = os.popen("hostname -I").read().split()
+    if len(hostname_cmd) >= 1:
+        if hostname_cmd[0] != "172.17.0.1":
+            local_ip = hostname_cmd[0]
+        else:
+            local_ip = "Unable"
+    else:
+        local_ip = "Unable"
+    ssid_cmd = os.popen("iwgetid").read().split('"')
+    if len(ssid_cmd) >= 2:
+        ssid = ssid_cmd[1]
+    else:
+        ssid = "NO WIFI"
+
+    if ssid != last_ssid or local_ip != last_local_ip:
+        image, draw = draw_ssid(image, draw, height, width, ssid, local_ip)
+    else:
+        if not static_network:
+            image, draw = draw_ssid(image, draw, height, width, ssid, local_ip)
+        else:
+            image = static_network.copy()
+            draw = ImageDraw.Draw(image)
+    return image, draw
+
 all_planes = {}
 total_planes = 0
 
-def frame_boarder(image, draw, height, width):
+def frame_boarder(draw, height, width):
     global planes_count
+
 
     #Title Bar
 
-    draw.text((0, 0), "ADS-B Monitor", font=font_bold_10)
-    draw.line([(0, (spacing_height_10*1)+offset_10), (width, (spacing_height_10*1)+offset_10)], width=1)
 
     #Uptime
     current_time = datetime.now()
@@ -90,45 +163,34 @@ def frame_boarder(image, draw, height, width):
     tb_offset_x = tb_offset_x - spacing_width_10*5
     draw.text((tb_offset_x, 0), "TEMP", font=font_bold_10)
 
-    #Plane Image
-    image.paste(bmp, (offset_10, 38))
-    draw.line([(65, (spacing_height_10*1)+offset_10), (65, height-spacing_height_10*1-offset_10)], width=1)
 
     #Bottom Bar
-    bb_offset_x = 0
-    bb_offset_y = height-spacing_height_10*1
-    draw.line([(bb_offset_x, bb_offset_y-offset_10), (width, bb_offset_y-offset_10)], width=1)
+
 
     #Total Planes
+    bb_offset_x = 0
+    bb_offset_y = height-spacing_height_10*1
     draw.text((bb_offset_x, bb_offset_y), "TOTAL", font=font_bold_10)
     bb_offset_x = bb_offset_x + spacing_width_10*6
     draw.text((bb_offset_x, bb_offset_y), str(total_planes), font=font_reg_10)
 
-    #SSID + IP
-    hostname_cmd = os.popen("hostname -I").read().split()
-    if len(hostname_cmd) >= 1:
-        local_ip = hostname_cmd[0]
-    else:
-        local_ip = "Unable"
-    ssid_cmd = os.popen("iwgetid").read().split('"')
-    if len(ssid_cmd) >= 2:
-        ssid = ssid_cmd[1]
-    else:
-        ssid = "NO WIFI"
-
-    bb_offset_x = width-spacing_width_10*len(local_ip)
-    draw.text((bb_offset_x, bb_offset_y), local_ip, font = font_reg_10)
-    bb_offset_x = bb_offset_x - spacing_width_10*(len(ssid)+1)
-    draw.text((bb_offset_x, bb_offset_y), ssid, font = font_bold_10)
-
 client = httpx.Client(http2=True, base_url="http://127.0.0.1/tar1090")
 
-def frame_planes(image, draw, height, width):
+def frame_planes(draw, height, width):
     global all_planes
     global total_planes
     global client
     planes = []
-    r = client.get("/data/aircraft.json")
+
+    try:
+        r = client.get("/data/aircraft.json")
+    except httpx.RemoteProtocolError:
+        for i in range(1,3):
+            print("Retry in "+str(i))
+            r = client.get("/data/aircraft.json")
+            if r.status_code == httpx.codes.OK:
+                break
+
     if r.status_code == httpx.codes.OK:
         json = r.json()
         planes = json["aircraft"]
@@ -182,10 +244,10 @@ def frame_planes(image, draw, height, width):
 
 def frame(epd):
     print("Framing")
-    image = Image.new('1', (epd.height, epd.width), 255)
-    draw = ImageDraw.Draw(image)
-    frame_boarder(image, draw, epd.width, epd.height)
-    frame_planes(image, draw, epd.width, epd.height)
+    image, draw = frame_static(epd.width, epd.height)
+    image, draw = frame_ssid(image, draw, epd.width, epd.height)
+    frame_boarder(draw, epd.width, epd.height)
+    frame_planes(draw, epd.width, epd.height)
     epd.displayPartial(epd.getbuffer(image))
     return True
 
